937c650cd99c9a598c2f4a092da1551c07213f35
LeonIfran <leonsifran@gmail.com>
2018-04-14 23:39:00 -0300

    sddsa

--- guia3/ejercicio28/enigma.php
+++ guia3/ejercicio28/enigma.php
@@ -32,18 +32,43 @@ class Enigma
 
     public static function Desencriptar($path)
     {
-        $auxDese;
-        
+/*         $auxDese;
+        $auxFinal=NULL;
         $lector = fopen($path,"r");
         
         while (!feof($lector)) 
         {
             $auxLinea = fgets($lector);
-            $auxDese = explode(" ",$auxLinea);
-        }
+            //echo $auxLinea."<br>";
+            $auxLinea = trim($auxLinea);
+            //echo $auxLinea."<br>";
+            if ($auxLinea!="" && $auxLinea!=" " && $auxLinea!=NULL) 
+            {
+                $auxDese = $auxLinea-200;
+            }
+            echo $auxDese."<br>";
 
+        }
+        fclose($lector); */
+                $auxDese;
+        
+        $lector = fopen($path,"r");
+        
+        
+            $auxLinea = fgets($lector);
+            $auxDese = explode(" ",$auxLinea);
+            $aux;
+            $len = count($auxDese);
+            for ($i=0; $i < $len; $i++)
+            {   
+                $aux[$i] = chr($auxDese[$i]-200);
+            }
+            $aux = implode("",$aux);
+            return $aux;
+        
     }
 }
 $men = "asdf";
 Enigma::Encriptar($men,"../misArchivos/contrasenia.txt");
+echo Enigma::Desencriptar("../misArchivos/contrasenia.txt");
 ?>
